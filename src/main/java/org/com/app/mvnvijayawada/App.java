823ed253247e592a2b816f7f5ba2e9c01bee3ea4
mohan <mohan.chanti4@gmail.com>
2019-07-20 11:33:32 +0530

    new changes

---
+++ src/main/java/org/com/app/mvnvijayawada/App.java
@@ -0,0 +1,13 @@
+package org.com.app.mvnvijayawada;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "mvnvijayawada" );
+    }
+}
